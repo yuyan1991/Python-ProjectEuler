18bd25fbfa4cf705873eb77853c3b4be560c1871
yuyan <yuyan1991@gmail.com>
2017-12-18 11:26:54 +0800

    Add PE0005.py: In fact, the answer is 2520 * 11 * 13 * (16/8) * 17 * 19.

---
+++ PE0005.py
@@ -0,0 +1,12 @@
+# config: utf-8 -*-
+x=2520
+while True:
+    okay=True
+    for i in [num for num in range(2,21)]:
+        if x%i>0:
+            okay=False
+            break
+    if okay:
+        print(x)
+        break
+    x+=2520 # we increased by 2520 to fasten the speed of finding the solution
