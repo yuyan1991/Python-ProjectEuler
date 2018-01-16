31c845a40c501241e5412c93085e6d7b9be58f2e
yuyan <yuyan1991@gmail.com>
2018-01-16 10:49:15 +0800

    Fix the bug on PE0034.py

--- PE0034.py
+++ PE0034.py
@@ -9,26 +9,17 @@ ans = 0
 
 def search(cur, v):
     global ans
-    if cur + F[maxn-1] < v * 10:
-        return
     if cur == v:
         print(v)
         ans+=v
-    print('(%d,%d)'%(cur,v))
+    if cur + F[maxn-1] < v * 10:
+        # print('(%d,%d)'%(cur,v))
+        return
     for i in range(maxn):
         search(cur + F[i], v * 10 + i)
-    # search(cur + F[0], v * 10 + 0)
-    # search(cur + F[1], v * 10 + 1)
-    # search(cur + F[2], v * 10 + 2)
-    # search(cur + F[3], v * 10 + 3)
-    # search(cur + F[4], v * 10 + 4)
-    # search(cur + F[5], v * 10 + 5)
-    # search(cur + F[6], v * 10 + 6)
-    # search(cur + F[7], v * 10 + 7)
-    # search(cur + F[8], v * 10 + 8)
-    # search(cur + F[9], v * 10 + 9)
 
 if __name__=='__main__':
     for i in range(1, maxn):
         search(F[i], i)
-    print('Ans=', ans)
+    print(F)
+    print('Ans=', ans -1 - 2)
