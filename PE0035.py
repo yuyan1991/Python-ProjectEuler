ad8346e96bfe98a73d892daf9367914cf03cdc0e
yuyan <yuyan1991@gmail.com>
2018-01-17 15:04:33 +0800

    Update PE0035.py: faster, around 2 seconds

--- PE0035.py
+++ PE0035.py
@@ -1,12 +1,14 @@
 #-*- coding: utf-8 -*-
 
 maxn = 1000000
-P = set([2])
+P = [2]
 
 def prime_generator(high):
     for n in range(3, high, 2):
         okay = True
         for p in P:
+            if p * p > n:
+                break
             if n % p == 0:
                 okay = False
                 break
@@ -14,20 +16,21 @@ def prime_generator(high):
             yield n
 
 if __name__=='__main__':
-	for n in prime_generator(maxn):
-		P.add(n)
-	ans=0
-	for p in P:
-		l = len(str(p))
-		x = p
-		while True:
-			x = (x%10)*10**(l-1)+x//10
-			if not (x in P):
-				break
-			if x == p:
-				break
-		if x==p:
-			ans+=1
-			print(p, end=' ')
-	print('')
-	print(ans)
+    for n in prime_generator(maxn):
+        P.append(n)
+    P = set(P)
+    ans=0
+    for p in P:
+        l = len(str(p))
+        x = p
+        while True:
+            x = (x%10)*10**(l-1)+x//10
+            if not (x in P):
+                break
+            if x == p:
+                break
+        if x==p:
+            ans+=1
+            print(p, end=' ')
+    print('')
+    print(ans)
