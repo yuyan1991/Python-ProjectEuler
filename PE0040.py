906f3bdd5c7ab1cd68f44c9c0cf6e8d9b522c419
yuyan <yuyan1991@gmail.com>
2018-01-25 12:27:09 +0800

    Add PE0040.py

---
+++ PE0040.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+maxn = 1000000
+
+def solve():
+	s = ''
+	for i in range(1, maxn):
+		s += str(i)
+	ans = 1
+	x = 1
+	while x <= maxn:
+		ans *= int(s[x - 1])
+		print(s[x - 1])
+		x *= 10
+	print(ans)
+
+if __name__=='__main__':
+	solve()
