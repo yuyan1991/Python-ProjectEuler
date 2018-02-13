9bebdfdabf7dec33ffe62aafd1c1d71fa06a9901
yuyan <yuyan1991@gmail.com>
2018-02-13 10:43:21 +0800

    Correct PE0055.py

--- PE0055.py
+++ PE0055.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
 maxn = 10000
-numOfIterations = 50 + 1
+numOfIterations = 50
 
 if __name__=='__main__':
-	ans=0
-	for number in range(maxn):
-		n = number
-		okay = True
-		for j in range(numOfIterations):
-			if str(n)==str(n)[::-1]:
-				okay = False
-				break
-			n = n + int(str(n)[::-1])
-		ans += okay
-	print(ans)
-	# print(int(str(ans)[::-1]))
+    ans=0
+    for number in range(maxn):
+        n = number
+        okay = True
+        for j in range(numOfIterations):
+            n = n + int(str(n)[::-1])
+            if str(n) == str(n)[::-1]:
+                okay = False
+                break
+        ans += okay
+    print(ans)
+    # print(int(str(ans)[::-1]))
