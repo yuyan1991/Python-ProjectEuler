972cd9265406bb5a3f0dd1a8a6e78a3f0671613a
yuyan <yuyan1991@gmail.com>
2018-02-28 14:17:07 +0800

    Update PE0062: Use reduce to convert the list to str instead of the loop

--- PE0062.py
+++ PE0062.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 
+from functools import reduce
+
 maxn = 10000
 d = {}
 ans = maxn
 
 def solve():
-	for n in range(maxn):
-		res = n ** 3
-		s = ''
-		for c in sorted(str(res)):
-			s += c
-		if d.get(s) is None:
-			d[s]=[n,1]
-		else:
-			d[s][1]+=1
+    for n in range(maxn):
+        s = reduce(lambda x, y: y + x, sorted(str(n**3)))
+        if d.get(s) is None:
+            d[s]=[n,1]
+        else:
+            d[s][1]+=1
 
 if __name__=='__main__':
-	solve()
-	for v in d.values():
-		if v[1]==5 and v[0]<ans:
-			ans = v[0]
-	print('%d: %d'%(ans, ans**3))
+    solve()
+    for v in d.values():
+        if v[1]==5 and v[0]<ans:
+            ans = v[0]
+    print('%d: %d'%(ans, ans**3))
